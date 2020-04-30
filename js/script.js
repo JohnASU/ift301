7763924c09d49fab337d0a879da0bd48b1fc145c
John <john@example.com>
2020-04-30 09:38:32 -0700

    Built layout for most pages. added background gradient

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var visitorName;
-var greeting;
-
-function idVisitor() {
-  visitorName = document.getElementById("visitorName").value;
-  greeting = "Hello, " + visitorName + ". We are glad to meet you.";
-}
-
-document.getElementById("greeting").innerHTML = greeting;
